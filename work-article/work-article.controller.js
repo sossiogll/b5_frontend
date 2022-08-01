f8fed2317c6650a650aa10c2fe8ae8c99558564b
dagui <enzoguida1997@gmail.com>
2022-08-01 12:17:48 +0200

    -Aggiornamento Bootstrap -Aggiornamento e miglioramento dei css delle pagine -Responsive Lavori, Magazine, Lavori singoli e Magazine singoli -Sticky "Condividi" nell'articolo singolo -Miglioramento e rifinitura Articolo singolo

--- work-article/work-article.controller.js
+++ work-article/work-article.controller.js
@@ -8,7 +8,8 @@
     WorkArticleController.$inject = ['$rootScope'];
     function WorkArticleController($rootScope) {
         var vm = this;
-
+        vm.articleinfo= JSON.parse('{"data":{"id":3,"title":"Restauro dell\u2019Anfiteatro Flavio (Colosseo)","slug":"restauro-dellanfiteatro-flavio-colosseo","content":"<p>Complessit\u00e0 e spazialit\u00e0 tridimensionale sono state dunque le direttrici che hanno indirizzato la fase della progettazione esecutiva.<\/p>","posted_at":"2022-07-30T17:06:25+02:00","comments_count":0,"category_id":2,"custom_fields_values":{"localita":"Roma","importo":"8,0000","impresa":"Altera S.p.A."}}}');
+        console.log(vm.articleinfo.data);
         initController();
 
         function initController() {
